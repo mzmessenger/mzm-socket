3482367dbbb34fa27ca440b878c3c947081e0974
kohta ito <kohta110@gmail.com>
2019-07-27 13:46:34 +0900

    cluster

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv'
 config()
 
+export const WORKER_NUM = 2
 export const SOCKET_LISTEN = 3000
 
 export const INTERNAL_API_URL = process.env.INTERNAL_API
--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,7 @@
+import cluster from 'cluster'
 import WebSocket from 'ws'
 import uuid from 'uuid/v4'
-import { SOCKET_LISTEN } from './config'
+import { WORKER_NUM, SOCKET_LISTEN } from './config'
 import logger from './lib/logger'
 import redis from './lib/redis'
 import { requestSocketAPI } from './lib/req'
@@ -13,52 +14,69 @@ type PostData = {
   payload: { user: string; twitterUserName: string }
 }
 
-redis.on('connect', async () => {
-  const wss = new WebSocket.Server({
-    port: SOCKET_LISTEN
+if (cluster.isMaster) {
+  for (let i = 0; i < WORKER_NUM; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    const s = signal || code
+    logger.info(`exit worker #${worker.process.pid} (${s})`)
+    cluster.fork()
   })
+} else {
+  redis.on('connect', async () => {
+    const wss = new WebSocket.Server(
+      {
+        port: SOCKET_LISTEN
+      },
+      () => {
+        logger.info('Listening on', SOCKET_LISTEN)
+      }
+    )
 
-  consume()
+    consume()
 
-  wss.on('connection', async function connection(ws: ExtWebSocket, req) {
-    const user: string = req.headers['x-user-id'] as string
-    if (!user) {
-      ws.close()
-      return
-    }
-    const id = uuid()
-    ws.id = id
-    saveSocket(id, user, ws)
-    const twitterUserName = req.headers['x-twitter-user-name'] as string
-
-    const data: PostData = {
-      cmd: 'socket:connection',
-      payload: { user, twitterUserName }
-    }
-    requestSocketAPI(data, user, id).catch(e => {
-      logger.error('[post:error]', e)
-    })
-
-    ws.on('message', async function incoming(message) {
-      if (message === 'pong') {
+    wss.on('connection', async function connection(ws: ExtWebSocket, req) {
+      const user: string = req.headers['x-user-id'] as string
+      if (!user) {
+        ws.close()
         return
       }
-      try {
-        await requestSocketAPI(message, user, id)
-      } catch (e) {
-        logger.error('[post:error]', e)
+      const id = uuid()
+      ws.id = id
+      saveSocket(id, user, ws)
+      const twitterUserName = req.headers['x-twitter-user-name'] as string
+
+      const data: PostData = {
+        cmd: 'socket:connection',
+        payload: { user, twitterUserName }
       }
+      requestSocketAPI(data, user, id).catch(e => {
+        logger.error('[post:error]', e)
+      })
+
+      ws.on('message', async function incoming(message) {
+        if (message === 'pong') {
+          return
+        }
+        try {
+          await requestSocketAPI(message, user, id)
+        } catch (e) {
+          logger.error('[post:error]', e)
+        }
+      })
+
+      ws.on('close', function close() {
+        logger.info('closed:', user, ws.id)
+        removeSocket(ws.id, user)
+      })
     })
 
-    ws.on('close', function close() {
-      logger.info('closed:', user, ws.id)
-      removeSocket(ws.id, user)
-    })
+    setInterval(() => {
+      wss.clients.forEach(ws => {
+        ws.send('ping')
+      })
+    }, 50000)
   })
-
-  setInterval(() => {
-    wss.clients.forEach(ws => {
-      ws.send('ping')
-    })
-  }, 50000)
-})
+}
